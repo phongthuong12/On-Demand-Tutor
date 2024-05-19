e2b70f668b87f6ec6ab83e30cdad47a3286cb428
phongthuong12 <90915750+phongthuong12@users.noreply.github.com>
2024-05-19 17:39:00 +0700

    update db

---
+++ On_Demand_Tutor.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE On_Demand_Tutor;
+USE On_Demand_Tutor;
+
+CREATE TABLE roles(
+    role_id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE users(
+    user_id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15),
+    address VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)  
+);
+
+CREATE TABLE subjects (
+    subject_id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE grade (
+    grade_id INT PRIMARY KEY IDENTITY(1,1),
+    subject_id INT NOT NULL,
+    grade_level INT NOT NULL, 
+    description TEXT,
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+);
+
+CREATE TABLE tutors (
+    tutor_id INT PRIMARY KEY IDENTITY(1,1), 
+    first_name VARCHAR(255) NOT NULL,         
+    last_name VARCHAR(255) NOT NULL,          
+    email VARCHAR(255) NOT NULL UNIQUE,       
+    password VARCHAR(100) NOT NULL,           
+    phone_number VARCHAR(15),                 
+    address VARCHAR(255),                    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY IDENTITY(1,1),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15),
+    address VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
